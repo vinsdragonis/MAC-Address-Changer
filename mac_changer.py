9459de7703c9cf1663363c2f28362f418d25c40a
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2020-10-18 01:21:17 +0530

    Update - 1
    This new update supports command line args for interface and MAC address.

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("interface\n>")
-newMac = input("address\n>")
+parser = optparse.OptionParser()
 
-print("Changed MAC address for " + interface + " is set to = " + newMac)
+parser.add_option("-i", "--interface", dest="interface", help="use this to set network interface")
+parser.add_option("-m", "--mac", dest="new_mac", help="use this to set new MAC address for the network interface")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print("Changed MAC address for " + interface + " is set to = " + new_mac)
 
 subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw","ether", newMac])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
