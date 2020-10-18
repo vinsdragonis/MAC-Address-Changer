0f5f110588dfd4a072d438aa50039e5291487261
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2020-10-18 18:31:19 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -23,12 +24,32 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac):
-    print("Changed MAC address for " + interface + " is set to = " + new_mac)
+    print("[+] Changing MAC address for " + interface + " to: " + new_mac)
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+
+    mac_address = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
+    if mac_address:
+        return mac_address.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
 option = get_arguments()
+
+current_mac = get_current_mac(option.interface)
+print("Previous MAC address: " + str(current_mac))
+
 change_mac(option.interface, option.new_mac)
+
+current_mac = get_current_mac(option.interface)
+if current_mac == option.new_mac:
+    print("[+] Successfully changed MAC address to " + str(current_mac))
+else:
+    print("[-] MAC address not changed")
